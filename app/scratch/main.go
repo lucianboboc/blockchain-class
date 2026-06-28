f9f66b91423345fe7af1ceab6f71c337f6282e78
Lucian Boboc <info@lucianboboc.com>
2026-06-28 12:38:01 +0200

    scratch digital signatures

---
+++ app/scratch/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+// Tx is the transactional information between two parties.
+type Tx struct {
+	FromID string `json:"from"`  // Ethereum: Account sending the transaction. Will be checked against signature.
+	ToID   string `json:"to"`    // Ethereum: Account receiving the benefit of the transaction.
+	Value  uint64 `json:"value"` // Ethereum: Monetary value received from this transaction.
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func run() error {
+	//privateKey, err := crypto.GenerateKey()
+	//if err != nil {
+	//	return err
+	//}
+
+	tx := Tx{
+		FromID: "Lucian",
+		ToID:   "Arthur",
+		Value:  1000,
+	}
+
+	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
+	if err != nil {
+		return fmt.Errorf("unable to load private key for node: %w", err)
+	}
+
+	data, err := json.Marshal(tx)
+	if err != nil {
+		return fmt.Errorf("unable to marshal: %w", err)
+	}
+
+	// Hash the stamp and txHash together in a final 32 byte array
+	// that represents the data.
+	v := crypto.Keccak256(data)
+
+	sig, err := crypto.Sign(v, privateKey)
+	if err != nil {
+		return fmt.Errorf("unable to sign: %w", err)
+	}
+
+	sigHash := hexutil.Encode(sig)
+
+	fmt.Println(sigHash)
+	return nil
+}
